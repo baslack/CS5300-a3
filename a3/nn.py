b6e905ce2b3a5a5595656e504209462686627bf8
Benjamin Slack <iam@nimajneb.com>
2018-06-03 00:39:38 -0400

    working dnn

--- a3/nn.py
+++ a3/nn.py
@@ -7,76 +7,94 @@ def nn_model_fn(features: tf.data.Dataset,
                 labels: tf.data.Dataset,
                 mode: tf.estimator.ModeKeys,
                 params: Dict) -> tf.estimator.EstimatorSpec:
+    # input = tf.layers.InputSpec(dtype=tf.float32, shape=(-1, 3072))
     dense1 = tf.layers.dense(features,
                              units=params["d1_units"],
                              activation=params["activation"],
                              use_bias=params["use_bias"],
                              # kernel_initializer=params["kernel_init"],
                              # bias_initializer=params["bias_init"],
-                             name="dense1")
+                             # name="dense1"
+                             )
     dense2 = tf.layers.dense(dense1,
                              units=params["d2_units"],
                              activation=params["activation"],
                              use_bias=params["use_bias"],
                              # kernel_initializer=params["kernel_init"],
                              # bias_initializer=params["bias_init"],
-                             name="dense2")
+                             # name="dense2"
+                             )
     dense3 = tf.layers.dense(dense2,
                              units=params["d3_units"],
                              activation=params["activation"],
                              use_bias=params["use_bias"],
                              # kernel_initializer=params["kernel_init"],
                              # bias_initializer=params["bias_init"],
-                             name="dense3")
+                             # name="dense3"
+                             )
     out = tf.layers.dense(dense3,
                           units=10,
-                          activation=tf.nn.softmax,
+                          activation=None,
                           use_bias=params["use_bias"],
                           # kernel_initializer=params["kernel_init"],
                           # bias_initializer=params["bias_init"],
-                          name="out")
-    out_reshape = tf.reshape(out, (-1, 1))
-    loss = tf.losses.sparse_softmax_cross_entropy(labels, out_reshape)
-    # loss_hook = tf.train.SummarySaverHook(
-    #     save_steps=100,
-    #     output_dir=params["logdir"],
-    #     summary_op=loss
-    # )
+                          # name="out"
+                          )
+
+    pred_class = tf.argmax(out, 1)
+    predictions = {
+        'class_ids': pred_class[:, tf.newaxis],
+        'probabilities': tf.nn.softmax(out),
+        'logits': out,
+    }
+
+    loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=out)
+
+    pred_one_hot = tf.reshape(tf.one_hot([pred_class], depth=10, on_value=1, off_value=0), (-1,))
+
+    # Compute evaluation metrics.
+    accuracy = tf.metrics.accuracy(labels=labels,
+                                   predictions=pred_one_hot,
+                                   name='acc_op')
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy[1])
+
     opt = tf.train.MomentumOptimizer(params["learning_rate"],
                                      params["momentum"])
-    train_op = opt.minimize(loss)
+
+    train_op = opt.minimize(loss, global_step=tf.train.get_global_step())
+
     return tf.estimator.EstimatorSpec(
         mode=mode,
-        predictions=out,
+        predictions=predictions,
         loss=loss,
         train_op=train_op,
-        # training_hooks=[loss_hook]
+        eval_metric_ops=metrics
     )
 
 
-BATCHSIZE = 128
+BATCHSIZE = 2048
 
 
 def nn_train_input_fn() -> tf.data.Dataset:
     train_x, train_y, _, _, _ = import_cifar(data_path)
     train_x, train_y = prep_dataset(train_x, train_y, bReshape=False)
     train_x = train_x.map(lambda x: tf.reshape(x, (-1, 3072)))
-    train_y = train_y.map(lambda y: tf.reshape(y, (-1, 1)))
     temp_dataset = tf.data.Dataset.zip((train_x, train_y))
     temp_dataset.shuffle(-1)
     temp_dataset.batch(BATCHSIZE)
-    # features = temp_dataset.map(lambda x, y: x)
-    # labels = temp_dataset.map(lambda x, y: y)
     return temp_dataset
 
 
 def nn_eval_input_fn() -> tf.data.Dataset:
     _, _, test_x, test_y, _ = import_cifar(data_path)
     test_x, test_y = prep_dataset(test_x, test_y, bReshape=False)
+    test_x = test_x.map(lambda x: tf.reshape(x, (-1, 3072)))
     return tf.data.Dataset.zip((test_x, test_y))
 
 
 if __name__ == "__main__":
+    _, _, _, _, names = import_cifar(data_path)
     temp_dir = "../tmp/nn_test"
     params = {
         "d1_units": 256,
@@ -84,41 +102,17 @@ if __name__ == "__main__":
         "d3_units": 64,
         "activation": tf.nn.leaky_relu,
         "use_bias": True,
-        "kernel_init": tf.initializers.random_normal(0, 0.05),
-        "bias_init": tf.initializers.random_normal(0, 0.05),
+        # "kernel_init": tf.initializers.random_normal(0, 0.05),
+        # "bias_init": tf.initializers.random_normal(0, 0.05),
         "learning_rate": 0.001,
-        "momentum": 0.0005,
+        "momentum": 0.9,
         "logdir": temp_dir
     }
     nn = tf.estimator.Estimator(nn_model_fn,
                                 model_dir=temp_dir,
                                 params=params)
 
-    nn.train(nn_train_input_fn, steps=100)
-
-    # train = nn.train(nn_train_input_fn, steps=100)
-    #
-    # eval = nn.evaluate(nn_eval_input_fn)
-    #
-    # _, _, _, _, names = import_cifar(data_path)
-    # pred = nn.predict(nn_eval_input_fn, predict_keys=names)
-    #
-    # writer = tf.summary.FileWriter(temp_dir)
-    # loss_sum = tf.summary.scalar("loss_sum", "loss")
-    # writer.add_summary(loss_sum)
-    #
-    # init_global = tf.global_variables_initializer()
-    # init_local = tf.local_variables_initializer()
-    # with tf.Session() as sess:
-    #     sess.run(init_global)
-    #     sess.run(init_local)
-    #     count = 0
-    #     while count < 10000:
-    #         while True:
-    #             count += 1
-    #             try:
-    #                 sess.run(train)
-    #             except:
-    #                 # sess.run(eval)
-    #                 break
-    #     # sess.run(pred)
+    for i in range(10):
+        nn.train(nn_train_input_fn, steps=5000)
+        nn.evaluate(nn_eval_input_fn)
+    preds = nn.predict(nn_eval_input_fn, predict_keys=names)
