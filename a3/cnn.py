732c68f4a704984b293edfb5d1fb5ddf9c6ce5a3
Benjamin Slack <iam@nimajneb.com>
2018-06-03 18:25:37 -0400

    temp fix shuffle sampling

--- a3/cnn.py
+++ a3/cnn.py
@@ -1,4 +1,156 @@
-#TODO: create CNN Model
+import tensorflow as tf
+from typing import Dict
+from a3.cifar import import_cifar, prep_dataset, data_path
 
-def cnn_model_fn(features, labels, mode, config):
-    pass
+
+def cnn_model_fn(features: tf.Tensor,
+                 labels: tf.Tensor,
+                 mode: tf.estimator.ModeKeys,
+                 params: Dict) -> tf.estimator.EstimatorSpec:
+    weight_decay = 1e-4
+
+    # input_ = tf.reshape(features, (-1, 32, 32, 3))
+    # labels = tf.reshape(labels, (-1, 10))
+
+    # conv group 1
+    conv1 = tf.layers.separable_conv2d(features,
+                                       32,
+                                       3,
+                                       strides=1,
+                                       padding="same",
+                                       activation=params["activation"],
+                                       use_bias=params["use_bias"],
+                                       # pointwise_regularizer=tf.keras.regularizers.l2(weight_decay),
+                                       # depthwise_regularizer=tf.keras.regularizers.l2(weight_decay),
+                                       # bias_regularizer=tf.keras.regularizers.l2(weight_decay)
+                                       )
+    norm1 = tf.layers.batch_normalization(conv1)
+    dropout1 = tf.layers.dropout(norm1, rate=0.2)
+
+    # conv group 2
+    conv2 = tf.layers.conv2d(dropout1,
+                             64,
+                             3,
+                             strides=2,
+                             padding="same",
+                             activation=params["activation"],
+                             use_bias=params["use_bias"],
+                             # kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
+                             # bias_regularizer=tf.keras.regularizers.l2(weight_decay)
+                             )
+    norm2 = tf.layers.batch_normalization(conv2)
+    dropout2 = tf.layers.dropout(norm2, rate=0.3)
+
+    # conv group 3
+    conv3 = tf.layers.conv2d(dropout2,
+                             128,
+                             3,
+                             strides=2,
+                             padding="same",
+                             activation=params["activation"],
+                             use_bias=params["use_bias"],
+                             # kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
+                             # bias_regularizer=tf.keras.regularizers.l2(weight_decay)
+                             )
+    norm3 = tf.layers.batch_normalization(conv3)
+    dropout3 = tf.layers.dropout(norm3, rate=0.4)
+
+    flat = tf.layers.flatten(dropout3)
+
+    out = tf.layers.dense(flat,
+                          units=10,
+                          activation=None,
+                          use_bias=params["use_bias"],
+                          )
+
+    pred_class = tf.argmax(out, 1)
+    predictions = {
+        'class_ids': pred_class[:, tf.newaxis],
+        'probabilities': tf.nn.softmax(out),
+        'logits': out,
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=predictions,
+        )
+
+    loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=out)
+
+    pred_one_hot = tf.reshape(tf.one_hot([pred_class], depth=10, on_value=1, off_value=0), (-1, ))
+
+    # pred_one_hot = tf.reshape(tf.nn.softmax(out), (-1, ))
+
+    # Compute evaluation metrics.
+    accuracy = tf.metrics.accuracy(labels=labels,
+                                   predictions=pred_one_hot,
+                                   name='acc_op')
+    metrics = {'accuracy': accuracy}
+    tf.summary.scalar('accuracy', accuracy[1])
+
+    opt = tf.train.MomentumOptimizer(params["learning_rate"],
+                                     params["momentum"])
+
+    train_op = opt.minimize(loss, global_step=tf.train.get_global_step())
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op,
+            eval_metric_ops=metrics
+        )
+    else:
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            eval_metric_ops=metrics
+        )
+
+
+TRAIN_BATCHSIZE = 2048
+EVAL_BATCHSIZE = 10000
+TRAIN_SET_SIZE = 50000
+TEST_SET_SIZE = 10000
+TRAIN_ITER_SIZE = 10000
+
+
+def cnn_train_input_fn() -> tf.data.Dataset:
+    train_x, train_y, _, _, _ = import_cifar(data_path)
+    train_x, train_y = prep_dataset(train_x, train_y)
+    temp_dataset = tf.data.Dataset.zip((train_x, train_y))
+    temp_dataset = temp_dataset.shuffle(TRAIN_ITER_SIZE)
+    # temp_dataset = temp_dataset.batch(TRAIN_BATCHSIZE)
+    return temp_dataset
+
+
+def cnn_eval_input_fn() -> tf.data.Dataset:
+    _, _, test_x, test_y, _ = import_cifar(data_path)
+    test_x, test_y = prep_dataset(test_x, test_y)
+    temp_dataset = tf.data.Dataset.zip((test_x, test_y))
+    # temp_dataset = temp_dataset.batch(EVAL_BATCHSIZE)
+    return temp_dataset
+
+
+if __name__ == "__main__":
+    _, _, _, _, names = import_cifar(data_path)
+    temp_dir = "../tmp/cnn_test"
+    params = {
+        "activation": tf.nn.leaky_relu,
+        "use_bias": True,
+        "learning_rate": 0.001,
+        "momentum": 0.9,
+        "logdir": temp_dir
+    }
+    cnn = tf.estimator.Estimator(cnn_model_fn,
+                                 model_dir=temp_dir,
+                                 params=params)
+
+    for i in range(7):
+        cnn.train(cnn_train_input_fn, steps=10000)
+        cnn.evaluate(cnn_eval_input_fn)
+    preds = cnn.predict(cnn_eval_input_fn, predict_keys='class_ids')
+    for this in preds:
+        print(names[this['class_ids'][0]])
+        break
--- a3/nn.py
+++ a3/nn.py
@@ -7,38 +7,22 @@ def nn_model_fn(features: tf.data.Dataset,
                 labels: tf.data.Dataset,
                 mode: tf.estimator.ModeKeys,
                 params: Dict) -> tf.estimator.EstimatorSpec:
-    # input = tf.layers.InputSpec(dtype=tf.float32, shape=(-1, 3072))
-    dense1 = tf.layers.dense(features,
-                             units=params["d1_units"],
-                             activation=params["activation"],
-                             use_bias=params["use_bias"],
-                             # kernel_initializer=params["kernel_init"],
-                             # bias_initializer=params["bias_init"],
-                             # name="dense1"
-                             )
-    dense2 = tf.layers.dense(dense1,
-                             units=params["d2_units"],
-                             activation=params["activation"],
-                             use_bias=params["use_bias"],
-                             # kernel_initializer=params["kernel_init"],
-                             # bias_initializer=params["bias_init"],
-                             # name="dense2"
-                             )
-    dense3 = tf.layers.dense(dense2,
-                             units=params["d3_units"],
-                             activation=params["activation"],
-                             use_bias=params["use_bias"],
-                             # kernel_initializer=params["kernel_init"],
-                             # bias_initializer=params["bias_init"],
-                             # name="dense3"
-                             )
-    out = tf.layers.dense(dense3,
+
+    dropout = features
+
+    for units in params["units"]:
+        dense = tf.layers.dense(dropout,
+                                units,
+                                activation=params["activation"],
+                                use_bias=params["use_bias"],
+                                )
+        norm = tf.layers.batch_normalization(dense)
+        dropout = tf.layers.dropout(norm)
+
+    out = tf.layers.dense(dropout,
                           units=10,
                           activation=None,
                           use_bias=params["use_bias"],
-                          # kernel_initializer=params["kernel_init"],
-                          # bias_initializer=params["bias_init"],
-                          # name="out"
                           )
 
     pred_class = tf.argmax(out, 1)
@@ -47,6 +31,12 @@ def nn_model_fn(features: tf.data.Dataset,
         'probabilities': tf.nn.softmax(out),
         'logits': out,
     }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            predictions=predictions,
+        )
 
     loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=out)
 
@@ -64,16 +54,27 @@ def nn_model_fn(features: tf.data.Dataset,
 
     train_op = opt.minimize(loss, global_step=tf.train.get_global_step())
 
-    return tf.estimator.EstimatorSpec(
-        mode=mode,
-        predictions=predictions,
-        loss=loss,
-        train_op=train_op,
-        eval_metric_ops=metrics
-    )
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op,
+            eval_metric_ops=metrics
+        )
+    else:
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            # train_op=train_op,
+            eval_metric_ops=metrics
+        )
 
 
-BATCHSIZE = 2048
+TRAIN_BATCHSIZE = 2048
+EVAL_BATCHSIZE = 10000
+TRAIN_SET_SIZE = 50000
+TEST_SET_SIZE = 10000
+TRAIN_ITER_SIZE = 10000
 
 
 def nn_train_input_fn() -> tf.data.Dataset:
@@ -81,8 +82,8 @@ def nn_train_input_fn() -> tf.data.Dataset:
     train_x, train_y = prep_dataset(train_x, train_y, bReshape=False)
     train_x = train_x.map(lambda x: tf.reshape(x, (-1, 3072)))
     temp_dataset = tf.data.Dataset.zip((train_x, train_y))
-    temp_dataset.shuffle(-1)
-    temp_dataset.batch(BATCHSIZE)
+    temp_dataset = temp_dataset.shuffle(TRAIN_ITER_SIZE)
+    # temp_dataset = temp_dataset.batch(TRAIN_BATCHSIZE)
     return temp_dataset
 
 
@@ -90,20 +91,17 @@ def nn_eval_input_fn() -> tf.data.Dataset:
     _, _, test_x, test_y, _ = import_cifar(data_path)
     test_x, test_y = prep_dataset(test_x, test_y, bReshape=False)
     test_x = test_x.map(lambda x: tf.reshape(x, (-1, 3072)))
-    return tf.data.Dataset.zip((test_x, test_y))
+    temp_dataset = tf.data.Dataset.zip((test_x, test_y))
+    return temp_dataset
 
 
 if __name__ == "__main__":
     _, _, _, _, names = import_cifar(data_path)
     temp_dir = "../tmp/nn_test"
     params = {
-        "d1_units": 256,
-        "d2_units": 512,
-        "d3_units": 64,
+        "units": [2048, 2048, 2048],
         "activation": tf.nn.leaky_relu,
         "use_bias": True,
-        # "kernel_init": tf.initializers.random_normal(0, 0.05),
-        # "bias_init": tf.initializers.random_normal(0, 0.05),
         "learning_rate": 0.001,
         "momentum": 0.9,
         "logdir": temp_dir
@@ -112,7 +110,10 @@ if __name__ == "__main__":
                                 model_dir=temp_dir,
                                 params=params)
 
-    for i in range(10):
-        nn.train(nn_train_input_fn, steps=5000)
+    for i in range(7):
+        nn.train(nn_train_input_fn, steps=10000)
         nn.evaluate(nn_eval_input_fn)
-    preds = nn.predict(nn_eval_input_fn, predict_keys=names)
+    preds = nn.predict(nn_eval_input_fn, predict_keys='class_ids')
+    for this in preds:
+        print(names[this['class_ids'][0]])
+        break
